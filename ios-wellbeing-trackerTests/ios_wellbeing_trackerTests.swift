c04e2c9ff7de9c3adc71587fb80ea19118e24c5a
jannikpasch <jannik.pasch@gmail.com>
2025-12-01 22:12:37 +0100

    Initial Commit

---
+++ ios-wellbeing-trackerTests/ios_wellbeing_trackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  ios_wellbeing_trackerTests.swift
+//  ios-wellbeing-trackerTests
+//
+//  Created by Jannik Pasch on 01.12.25.
+//
+
+import Testing
+@testable import ios_wellbeing_tracker
+
+struct ios_wellbeing_trackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
